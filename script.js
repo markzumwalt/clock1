9767da790d53b74516632982bda1bd1e0354cad1
markzumwalt <markzumwalt@gmail.com>
2025-03-16 11:18:46 -0500

    add if hour

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ window.onload = function () {
 
 function updateTime() {
 	const format = number => `${ +!(number < 10) && '' }${number}`;
-	
+	if (hour >= 12) {
+        if (hour > 12) hour -= 12;
+        am_pm = "PM";
+    } else if (hour == 0) {
+        hr = 12;
 	let date = new Date();
 	
 	document.querySelector("#hour")  .innerHTML = format( date.getHours()   );
