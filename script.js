93f0ef902db77ee95a669c016896ceb7a5f2a4e3
markzumwalt <markzumwalt@gmail.com>
2025-03-16 11:34:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ function updateTime() {
 	
 	document.querySelector("#hour")  .innerHTML = format( date.getHours()   );
 	document.querySelector("#minute").innerHTML = format( date.getMinutes() );
+	document.querySelector("#second").innerHTML = format( date.getSeconds() );
+
+	document.querySelector("#date").innerHTML = (
+		[
+			date.getDate(),
+      ++date.getMonth(), 
+			date.getFullYear()
+		]
+			.map(format)
+			.join('/')
+	);
 	
-
-
+	const week = [
+		"Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+	];
+	
+	document.querySelector("#day").innerHTML = week[ date.getDay() ];
 }
